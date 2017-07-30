6c2771583768cfacf45ccb013afc6ed6b42bc6fc
unknown <aksmkr@gmail.com>
2017-07-30 13:08:25 +0530

    added index page and controller

---
+++ src/main/kotlin/com/firstapp/SpringWebApp/controllers/HomeController.kt
@@ -0,0 +1,13 @@
+package com.firstapp.SpringWebApp.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class HomeController {
+
+    @RequestMapping("")
+    fun home(): String {
+        return "index"
+    }
+}
